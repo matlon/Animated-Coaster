c0295b56395e5e5299c817bc4f4a944b58cda1d6
matlon <mattias.lonnerberg@gmail.com>
2017-07-16 23:31:01 +0200

    Added Project

---
+++ Assets/IgnoreCollider.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreCollider : MonoBehaviour {
+    public Collider otherCollider;
+    void Start()
+    {
+        Physics.IgnoreCollision(GetComponent<Collider>(), otherCollider);
+    }
+}
