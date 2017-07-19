c6472cdfda87ad71655c366d20641c63c4ce9e96
matlon <mattias.lonnerberg@gmail.com>
2017-07-19 15:12:39 +0200

    FBX files and cleanup

---
+++ Assets/Scripts/SetForceTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetForceTrigger : MonoBehaviour {
+
+    private GoForward forwardScript;
+    public float force;
+    public float targetSpeed;
+    public bool hasTargetSpeed;
+
+    void OnTriggerEnter(Collider other)
+    {
+        forwardScript = other.transform.parent.GetComponent<GoForward>();
+        forwardScript.force = force;
+        forwardScript.targetSpeed = targetSpeed;
+        forwardScript.hasTargetSpeed = hasTargetSpeed;
+    }
+}
