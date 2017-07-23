db7b49b3c1cd1a79d32f3a3d21d9536e1172fabb
adhag <adrian.haggvik@gmail.com>
2017-07-23 21:58:28 +0200

    chair uv and POV camera

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public float speed = 5.0f;
+    public float speed2 = 1.0f;
+    public Transform target;
+
+    void LateUpdate () {
+
+        transform.position = Vector3.Lerp(transform.position, target.position, speed * Time.deltaTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, target.rotation, speed2 * Time.deltaTime);
+       // transform.LookAt(target);
+    }
+}
